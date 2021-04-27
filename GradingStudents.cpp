c3f7fbd381ae8eca9466edc7337571df8041d2df
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-04-27 11:37:51 +0900

    Create GradingStudents.cpp

---
+++ GradingStudents.cpp
@@ -0,0 +1,54 @@
+vector<int> gradingStudents(vector<int> grades)
+{
+    for (int i = 0; i < grades.size(); ++i)
+    {
+        if (grades[i] > 37)
+        {
+            switch (grades[i] % 5)
+            {
+                case 3:
+                    grades[i] += 2;
+                    break;
+                    
+                case 4:
+                    grades[i] += 1;
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+    }
+    
+    return grades;    
+}
+
+
+vector<int> gradingStudents2(vector<int> grades)
+{
+	vector<int> result;
+
+	std::for_each(grades.begin(), grades.end(),
+		[&result](auto& score)
+		{
+			int round = 0;
+			int temp = score % 5;
+			if (temp != 0)
+				round = 5 - temp;
+
+			if (score > 37)
+			{
+				if (round < 3)
+					result.push_back(score + round);
+				else
+					result.push_back(score);
+			}
+			else
+			{
+				result.push_back(score);
+			}
+		}
+	);
+
+	return result;
+}
