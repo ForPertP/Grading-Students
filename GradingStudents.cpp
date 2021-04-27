6e41957f5e5aa96aae3985eb3301227dabf316f9
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-04-27 15:53:50 +0900

    Update GradingStudents.cpp

--- GradingStudents.cpp
+++ GradingStudents.cpp
@@ -29,26 +29,25 @@ vector<int> gradingStudents2(vector<int> grades)
 	vector<int> result;
 
 	std::for_each(grades.begin(), grades.end(),
-		[&result](auto& score)
-		{
-			int round = 0;
-			int temp = score % 5;
-			if (temp != 0)
-				round = 5 - temp;
+	[&result](auto& score)
+	{
+		int round = 0;
+		int temp = score % 5;
+		if (temp != 0)
+			round = 5 - temp;
 
-			if (score > 37)
-			{
-				if (round < 3)
-					result.push_back(score + round);
-				else
-					result.push_back(score);
-			}
+		if (score > 37)
+		{
+			if (round < 3)
+				result.push_back(score + round);
 			else
-			{
 				result.push_back(score);
-			}
 		}
-	);
+		else
+		{
+			result.push_back(score);
+		}
+	} );
 
 	return result;
 }
