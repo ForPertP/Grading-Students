49b8bc24acf98abd1ccfd3339f4a34e2c7c365ce
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-10-25 08:49:53 +0900

    Update GradingStudents.cpp

--- GradingStudents.cpp
+++ GradingStudents.cpp
@@ -1,3 +1,15 @@
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
 vector<int> gradingStudents(vector<int> grades)
 {
     for (int i = 0; i < grades.size(); ++i)
